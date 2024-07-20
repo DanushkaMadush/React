3950dbe95ba8a46a67484a6bbe27d69c8615dea7
DanushkaMadush <danushka@codeolima.com>
2024-07-20 09:00:45 +0530

    react app created

---
+++ src/MainComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainComponent () {
+    return(
+      <div>
+        <h1>This is a header</h1>
+        <h3>Another header</h3>
+        <ol>
+            <li>One</li>
+            <li>Two</li >
+            <li>Three</li>
+        </ol>
+      </div>
+    )
+  }
